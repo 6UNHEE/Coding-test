820723bb0a4876e67aac747d8f3a3dd9b5a50fe0
6UNHEE <155932203+6UNHEE@users.noreply.github.com>
2024-02-02 14:32:19 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        year = int(input())
+    except:
+        break
+    
+    if (year % 4 == 0) and ((year % 100 != 0) or (year % 400 ==0)):
+        print('1')
+    else:
+        print('0')
